16387e23148dce61379e229e7242200f6f7d4de9
Ryan Doty <rdoty@amazon.com>
2023-09-26 12:37:31 -0400

    documentation

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from kendra_bedrock_query import kendraSearch
 
 st.markdown("<h1 style='text-align: center; color: red;'>RAG with Amazon Kendra </h1>", unsafe_allow_html=True)
 
-
 def main():
+    # configuring default values for session state
     if "query_text" not in st.session_state:
         st.session_state["query_text"] = []
 
@@ -14,30 +14,31 @@ def main():
     if "past" not in st.session_state:
         st.session_state["past"] = []
 
-    # col1 = st.column([10], gap="medium")
-    # with col1:
     with st.container():
+        # primary container that has text input box
         input_text = st.text_input("Ask a question:",
                                    "",
                                    key="query_text",
                                    placeholder="What can I help you with?",
                                    on_change=clear_text()
                                    )
+        # if the user inputs a question change the session state
         user_input = st.session_state["query"]
 
         if user_input:
+            # after the user inputs a question, append it to the session state
             st.session_state.past.append(user_input)
+            # pass that question into the Kendra Search function located in the kendra_bedrock_query.py file
             st.write(kendraSearch(user_input))
-
+    # clear chat button
     with st.container():
         st.button("clear chat", on_click=clear_session)
-
-
+#  this function clears all of the text
 def clear_text():
     st.session_state["query"] = st.session_state["query_text"]
     st.session_state["query_text"] = ""
 
-
+# This function clears all of the data from the session
 def clear_session():
     for key in st.session_state.keys():
         del st.session_state[key]
--- kendra_bedrock_query.py
+++ kendra_bedrock_query.py
@@ -4,29 +4,50 @@ import os
 from dotenv import load_dotenv
 import json
 
+# loading in environment variables
 load_dotenv()
+# setting default session with AWS CLI Profile
 boto3.setup_default_session(profile_name=os.getenv('profile_name'))
 
 
 def kendraSearch(question):
+    """
+    Primary function that performs a kendra search, using the retrieve API and passes the kendra response into the
+    invoke LLM function.
+    :param question: The question the user inputs within app.py or the frontend
+    :return: Returns the final response of the LLM that was created by the invokeLLM function
+    """
+    # initiating kendra client
     kendra = boto3.client('kendra')
+    # performing the retrieve call against Kendra
     kendra_response = kendra.retrieve(
         IndexId=os.getenv('kendra_index'),  # Put INDEX in .env file
         QueryText=question,
         PageNumber=1,
         PageSize=15
     )
+    # passing in the original question, and various kendra responses as context into the LLM
     return invokeLLM(question, kendra_response)
 
 
 def invokeLLM(question, kendra_response):
+    """
+    This function takes in the question from the user, along with the Kendra reponses as context to generate an answer
+    for the user on the frontend.
+    :param question: The question the user is asking that was asked via the frontend input text box.
+    :param kendra_response: The response from the Kendra document retrieve query, used as context to generate a better
+    answer.
+    :return: Returns the final answer that will be provided to the end-user of the application who asked the original
+    question.
+    """
     # Setup Bedrock client
     bedrock = boto3.client('bedrock', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com')
-
+    # configure model specifics such as specific model
     modelId = 'anthropic.claude-v2'
     accept = 'application/json'
     contentType = 'application/json'
-
+    # prompt that is passed into the LLM with the Kendra Retrieval context and question
+    # TODO: FEEL FREE TO EDIT THIS PROMPT TO CATER TO YOUR USE CASE
     prompt_data = f"""
 Human:    
 Answer the following question to the best of your ability based on the context provided.
@@ -44,6 +65,8 @@ Context: {kendra_response}
 Assitant:
 
 """
+    # body of data with parameters that is passed into the bedrock invoke model request
+    # TODO: TUNE THESE PARAMETERS AS YOU SEE FIT
     body = json.dumps({"prompt": prompt_data,
                        "max_tokens_to_sample": 8191,
                        "temperature": 0,
@@ -51,11 +74,14 @@ Assitant:
                        "top_p": 0.5,
                        "stop_sequences": []
                        })
+    # Invoking the bedrock model with your specifications
     response = bedrock.invoke_model(body=body,
                                     modelId=modelId,
                                     accept=accept,
                                     contentType=contentType)
-
+    # the body of the response that was generated
     response_body = json.loads(response.get('body').read())
+    # retrieving the specific completion field, where you answer will be
     answer = response_body.get('completion')
+    # returning the answer as a final result, which ultimately gets returned to the end user
     return answer
